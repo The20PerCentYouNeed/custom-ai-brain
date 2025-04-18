21f5e3de3114e29eaaa0873e2450400742170ca7
Αποστόλης Μουστάκης <a.moustakis@netstudio.gr>
2025-04-18 14:59:34 +0300

    Adds .env file and builds the connection with the postgres sql database

---
+++ db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	DB, err = sql.Open("postgres", connStr)
+	// fmt.Println(DB)
+	if err != nil {
+		log.Fatal("Error opening DB: ", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Cannot connect to DB: ", err)
+	}
+
+	log.Println("Successfully connected to database")
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/The20PerCentYouNeed/custom-ai-brain/db"
 	"github.com/The20PerCentYouNeed/custom-ai-brain/routes"
 )
 
 func main() {
+	db.InitDB()
+
 	r := routes.SetupRouter()
 	r.Run(":8080")
 }
